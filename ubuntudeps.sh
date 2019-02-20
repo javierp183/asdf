83b9823b49a66ec5c5799501468409a215f3a7e9
Javier Pereyra <javier.pereyra183@gmail.com>
2019-02-20 14:25:55 -0300

    ubuntu deps script

---
+++ ubuntudeps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Very Simple install script to install standard dependencies for python on Ubuntu
+# Author: javier pereyra 
+# email: javier.pereyra183@gmail.com
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl
